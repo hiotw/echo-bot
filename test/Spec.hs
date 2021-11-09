18742537d2cef6af81472ff67a568a4f5bd613a4
Anatoliy Tolstikov <mrfunnysage@yandex.ru>
2021-11-09 14:12:07 +0800

    Add test for invalid handle

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,10 +2,12 @@ module Main where
 
 
 import Test.HUnit
+import System.Environment
 import Telegram.Test as T
 
 
 main :: IO ()
 main = do
+  arg <- getArgs
   counts <- runTestTT T.tests
   putStrLn $ showCounts counts
